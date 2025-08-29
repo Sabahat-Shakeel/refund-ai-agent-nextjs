d377c9195b25fa73dac179b749d40ac5bc0edbf4
syeda <sbtshakeel61@gmail.com>
2025-08-29 11:36:47 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,8 +45,8 @@ export default function Home() {
     setIsLoading(true);
 
     try {
-      
-      const response = await fetch("https://huggingface.co/spaces/sbht99/refund-agent/refund" , {  // Update to your backend URL in production
+
+      const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL ||"https://sbht99-refund-agent.hf.space/refund" , {  // Update to your backend URL in production
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: input }),
