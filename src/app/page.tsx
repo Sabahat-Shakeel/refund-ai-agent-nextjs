bd741f731388df1f5dfc8f2e114e7a3443e5912d
syeda <sbtshakeel61@gmail.com>
2025-09-03 11:27:51 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ export default function Home() {
             key={idx}
            
             className={`max-w-lg p-3 rounded-t-2xl  shadow-cyan-900 flex-1    ${ 
-              msg.role === 'user' ? 'bg-slate-800 ml-auto text-whitw/80' : 'bg-neutral-100 text-black/80 '
+              msg.role === 'user' ? 'bg-slate-400 ml-auto ' : 'bg-neutral-100 text-black/80 '
             } shadow-md`} 
           > <div className='flex-1'> <TbMessageChatbotFilled /> </div>
             
